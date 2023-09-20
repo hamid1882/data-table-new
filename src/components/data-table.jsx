7dccf78012319fd629665dfc197033f766e67789
hamid1882 <hussainhafeez1100@gmail.com>
2023-09-20 16:29:21 +0530

    initial commit

---
+++ src/components/data-table.jsx
@@ -0,0 +1,82 @@
+import React, { useState } from "react";
+import SearchBar from "./search-bar";
+import { tableData } from "../data";
+
+function DataTable() {
+    const [tableDataState, setTableDataState] = useState(tableData);
+
+    const onSort = (sortBy) => {
+        console.log(sortBy);
+        const sortedData = tableDataState.sort((a, b) =>
+            a[sortBy] > b[sortBy] ? -1 : 0
+        );
+
+        console.log(sortedData);
+
+        setTableDataState(sortedData);
+    };
+
+    return ( <
+        div className = "table-container" >
+        <
+        SearchBar / > { " " } <
+        table >
+        <
+        tr >
+        <
+        th >
+        <
+        input type = "checkbox" / >
+        <
+        /th>{" "} <
+        th >
+        <
+        div className = "idTitle" >
+        <
+        p > Id < /p> <button onClick={() => onSort("id")}> ⬇️ </button > { " " } <
+        /div>{" "} <
+        /th>{" "} <
+        th >
+        <
+        div className = "idTitle" >
+        <
+        p > First Name < /p>{" "} <
+        button onClick = {
+            () => onSort("firstName") } > ⬇️ < /button>{" "} <
+        /div>{" "} <
+        /th>{" "} <
+        th >
+        <
+        div className = "idTitle" > { " " } <
+        p > Second Name < /p>{" "} <
+        button onClick = {
+            () => onSort("lastName") } > ⬇️ < /button>{" "} <
+        /div>{" "} <
+        /th>{" "} <
+        th > { " " } <
+        div className = "idTitle" >
+        <
+        p > Age < /p> <button onClick={() => onSort("age")}> ⬇️ </button > { " " } <
+        /div>{" "} <
+        /th>{" "} <
+        th > Full Name < /th>{" "} <
+        /tr>{" "} {
+            tableDataState.map(({ id, firstName, lastName, age, fullName }) => ( <
+                tr key = { id } >
+                <
+                td >
+                <
+                input type = "checkbox" / >
+                <
+                /td>{" "} <
+                td > { id } < /td> <td> {firstName} </td > < td > { lastName } < /td>{" "} <
+                td > { age } < /td> <td> {fullName} </td > { " " } <
+                /tr>
+            ))
+        } { " " } <
+        /table>{" "} <
+        /div>
+    );
+}
+
+export default DataTable;
---
+++ src/data.js
@@ -0,0 +1,29 @@
+export const tableData = [{
+        id: 1,
+        firstName: "Mr",
+        lastName: "Anonymous",
+        age: 30,
+        fullName: "Mr. Anonymous",
+    },
+    {
+        id: 2,
+        firstName: "Mr",
+        lastName: "Barreck",
+        age: 20,
+        fullName: "Mr. Barreck",
+    },
+    {
+        id: 3,
+        firstName: "Mrs",
+        lastName: "Shweta",
+        age: 40,
+        fullName: "Mrs. Shweta",
+    },
+    {
+        id: 4,
+        firstName: "Mr",
+        lastName: "Jadeja",
+        age: 22,
+        fullName: "Mr. Jadeja",
+    },
+];
